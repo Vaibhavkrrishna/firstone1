b365e0f52f69e29bd50f6e079ea3126d6d907812
Vaibhavkrrishna <61645250+Vaibhavkrrishna@users.noreply.github.com>
2021-04-15 01:26:56 +0530

    Add files via upload

---
+++ IET_1..js
@@ -0,0 +1,23 @@
+function alertbox() {
+    alert("Thank You,Your Response has been recorded")
+}
+
+function cancel() {
+    alert("Proceed to cancel")
+}
+
+function first() {
+    alert("Enter your last name ")
+}
+
+function last() {
+    alert("Enter your last name ")
+}
+
+function mail() {
+    alert("Enter your e-mail id ")
+}
+
+function phone() {
+    alert("Enter your 10 digit phone number excluding country code")
+}
---
+++ IETlogin.js
@@ -0,0 +1,26 @@
+function alertbox() {
+    alert("Thank you,You will be redirected to a page shortly ")
+}
+
+function cancel() {
+    alert("Proceed to cancel")
+}
+function forgot() {
+    alert("You may reset your password using your registered e-mail id")
+}
+
+function first() {
+    alert("Enter your IET memebership number")
+}
+
+function last() {
+    alert("Enter your last name ")
+}
+
+function mail() {
+    alert("Enter your e-mail id ")
+}
+
+function phone() {
+    alert("Enter your password")
+}
